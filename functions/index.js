5e07f34e9e907d6c4c735702d4fbb66379ac0120
Who else <quinteger75@gmail.com>
2021-07-20 14:47:09 -0400

    added places root

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,25 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+
+exports.places_root = functions.https.onRequest((req, res) => {
+    functions.logger.info("places_root", {structuredData: true});
+    let api_key = 'AIzaSyAbjVN9byumQ2V44-rErf8CE6TyPq1WcSw'
+    let url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=' + req.query.input + '&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=' + api_key
+    //console.log(url);
+
+    // Make a request for a user with a given ID
+    axios.get(url)
+        .then(function (response) {
+            // handle success
+            res.send(response.data)
+        })
+        .catch(function (error) {
+            // handle error
+            res.send(error);
+        })
+        .then(function () {
+            // always executed
+        });
+  });
